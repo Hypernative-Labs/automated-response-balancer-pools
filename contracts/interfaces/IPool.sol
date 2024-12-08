60dcc094af379c91a374b34504ce2a08c6fa2232
Mithras <dimabrook79@gmail.com>
2024-12-08 12:10:20 +0200

    add: pool interface

---
+++ contracts/interfaces/IPool.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+interface IPool {
+    /**
+     * @notice Pause the pool: an emergency action which disables all pool functions.
+     * @dev This is a permissioned function that will only work during the Pause Window set during pool factory
+     * deployment (see `TemporarilyPausable`).
+     */
+    function pause() external;
+}
