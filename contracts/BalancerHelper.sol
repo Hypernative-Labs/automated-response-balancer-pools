f52046340b82cfae4cfe753f203fad26323651d6
vazi-hn <vazi@hypernative.io>
2025-02-19 17:18:15 +0200

    S1 - locked pragma to 0.8.25

--- contracts/BalancerHelper.sol
+++ contracts/BalancerHelper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity 0.8.25;
 
 import {IPool} from "./interfaces/IPool.sol";
 import {Enum} from "./GnosisSafeL2/common/Enum.sol";
