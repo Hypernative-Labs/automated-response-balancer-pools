813cc2f211209d574a3f081550ced7073ea1acec
Mithras <dimabrook79@gmail.com>
2025-02-27 10:56:43 +0200

    update: MockSafe

--- contracts/Mocks/MockSafe.sol
+++ contracts/Mocks/MockSafe.sol
@@ -4,12 +4,13 @@ pragma solidity 0.8.25;
 interface IBalancerHelper {
     function updateSafe(address newSafe) external;
     function updateVault(address newVault) external;
+    function pause(uint256 from, uint256 to) external;
+    function pauseAll() external;
 }
 
 /// @title MockSafe
 /// @dev immitates some Safe functionality
 contract MockSafe {
-    
     address public owner;
     address public module;
 
@@ -18,7 +19,7 @@ contract MockSafe {
         _;
     }
 
-    constructor(address newOwner){
+    constructor(address newOwner) {
         owner = newOwner;
     }
 
@@ -32,5 +33,26 @@ contract MockSafe {
 
     function setModule(address newModule) external onlyOwner {
         module = newModule;
+    }
+
+    function pause(uint256 from, uint256 to) external onlyOwner {
+        IBalancerHelper(module).pause(from, to);
+    }
+
+    function pauseAll() external onlyOwner {
+        IBalancerHelper(module).pauseAll();
+    }
+
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes memory data,
+        uint8 operation
+    ) external returns (bool success) {
+        if (operation == 0) {
+            (success, ) = to.call{value: value}(data);
+            return success;
+        }
+        return false;
     }
 }
